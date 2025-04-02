ad3a3e1a6519ed06b4ae4610c8eebf164559b445
Lasse Hammer Priebe <lasse.priebe@icloud.com>
2025-04-02 12:40:29 +0200

    fix action history

--- Sources/XCUITestAgent/UITestAgent/UITestAgent.swift
+++ Sources/XCUITestAgent/UITestAgent/UITestAgent.swift
@@ -56,6 +56,7 @@ open class UITestAgent {
         case .success, .failure:
             return false
         default:
+            actionHistory.append(nextActionSequence)
             return true
         }
     }
